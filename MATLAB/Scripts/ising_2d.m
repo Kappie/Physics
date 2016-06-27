866079fb22ec2891e42569506ea0447d644701f8
Kappie <ghkapteijns@gmail.com>
2016-06-27 16:46:23 +0200

    daily backup

--- MATLAB/Scripts/ising_2d.m
+++ MATLAB/Scripts/ising_2d.m
@@ -1,10 +1,12 @@
 function ising_2d
-
-J = -1;
 beta_crit = log(1 + sqrt(2)) / 2;
 
 end
 
 function m = exact_magnetization(beta)
 m = (1 - sinh(2*beta)^-4)^(1/8);
+end
+
+function Q = Q(s1, s2, beta, J)
+Q = exp(beta * J * s1 * s2);
 end
