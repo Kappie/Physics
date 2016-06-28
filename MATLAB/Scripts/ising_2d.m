8dd59aa1364a963df8ed9eba99e6fffdaa566417
Kappie <ghkapteijns@gmail.com>
2016-06-28 13:17:25 +0200

    verder

--- MATLAB/Scripts/ising_2d.m
+++ MATLAB/Scripts/ising_2d.m
@@ -3,8 +3,13 @@ function ising_2d
   beta_crit = log(1 + sqrt(2)) / 2; % ~0.44
   J = 1;
   chi = 5;
-  chi_init = 2;
-  construct_a(0.35)
+  chi_init = 3;
+
+  C = random_C();
+  T = random_T();
+  a = construct_a(0.35);
+
+  grow_C(C, T, a);
 
   % Works only for beta > beta_crit.
   function m = exact_magnetization(beta)
@@ -12,6 +17,27 @@ function ising_2d
   end
 
   function [T C] = perform_ctm_step()
+  end
+
+  function C = grow_C(C, T, a)
+    % Final order is specified so that the new tensor is ordered according to
+    % [d, chi, c, chi], with the pairs of c, chi corresponding to the reshaped
+    % legs of the new C.
+    C = ncon({C, T, T, a}, {[1, 2], [3, 1, -1], [4, 2, -2], [3, -3, -4, 4]}, ...
+    [1, 2, 3, 4], [-3 -1 -4 -2]);
+    combined_dimension = size(C, 1) * size(C, 2);
+    C = reshape(C, combined_dimension, combined_dimension)
+
+
+  end
+
+  % Keep only chi states of largest weight by performing singular value decomposition.
+  % Assumes A is symmetric.
+  function truncate_matrix(A, max_dimension)
+    [U, s, V] = svd(A)
+  end
+
+  function T = grow_T(T, a)
   end
 
   function Q = construct_Q(beta)
@@ -25,9 +51,9 @@ function ising_2d
   end
 
   function b = construct_b(beta)
-    g = construct_g()
+    g = construct_g();
     P = sqrt(construct_Q(beta));
-    b = ncon({P, P, P, P, g}, {[-1, 1], [-2, 2], [-3, 3], [-4, 4], [1, 2, 3, 4]})
+    b = ncon({P, P, P, P, g}, {[-1, 1], [-2, 2], [-3, 3], [-4, 4], [1, 2, 3, 4]});
   end
 
   function delta = construct_delta()
@@ -37,13 +63,24 @@ function ising_2d
   end
 
   function g = construct_g()
-    g = construct_delta()
-    g(2, 2, 2, 2) = -1
+    g = construct_delta();
+    g(2, 2, 2, 2) = -1;
   end
 
-  function t = random_t
+  function T = random_T
+    % Why do we not symmetrize in the physical dimension?
+    T = rand(2, chi_init, chi_init);
+    T(1,:,:) = random_symmetric_matrix(chi_init);
+    T(2,:,:) = random_symmetric_matrix(chi_init);
   end
 
-  function c = random_c
+  function C = random_C
+    C = random_symmetric_matrix(chi_init);
   end
+
+  function r = random_symmetric_matrix(dim)
+    r = rand(dim);
+    r = triu(r) + triu(r, 1)';
+  end
+
 end
