1cc014556359a0ab90c65a53cb633b71226a5e1e
Kappie <ghkapteijns@gmail.com>
2016-07-01 17:05:05 +0200

    gave up on this plotting package

--- MATLAB/Scripts/plot_order_parameter.m
+++ MATLAB/Scripts/plot_order_parameter.m
@@ -1,31 +1,3 @@
 function plot_order_parameter(temperatures, order_parameters)
-  plt = Plot(temperatures, order_parameters);
-  plt.LineStyle = {'none'};
-  % plt.Markers = {'o'};
-
-  plt.Title = 'Order parameter';
-  plt.XLabel = 'T';
-  plt.YLabel = 'm';
-  plt.Markers = {'d'};
-  plt.ShowBox = 'off';
-  plt.LineStyle = '--';
-
-  % f = 50;  % frequency
-  % Vm = 10; % peak
-  % phi = 0; % phase
-  %
-  % % generate the signal
-  % t = [0:0.0001:3/f];
-  % th = 2*pi*f*t;
-  % v = Vm*sin(th+phi);
-  %
-  % % plot it
-  % plt = Plot(t*1E3, v);
-  %
-  % plt.Title = 'Voltage as a function of time'; % plot title
-  % plt.XLabel = 'Time, t (ms)'; % xlabel
-  % plt.YLabel = 'Voltage, V (V)'; %ylabel
-
-  % plt.LineStyle = {'--'};
-  % plt.export('plotSimple1.png');
+  plot(temperatures, order_parameters, '*')
 end
