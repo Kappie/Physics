7a61a039c229a466af253f7e99aceef180fe3251
Kappie <ghkapteijns@gmail.com>
2016-09-15 16:14:08 +0200

    backup

--- MATLAB/Scripts/correlation_length.m
+++ MATLAB/Scripts/correlation_length.m
@@ -1,10 +1,9 @@
 function correlation_length = correlation_length(temperature, ~, T)
   % construct row of spins, and attach two rows together.
-  transfer_matrix = ncon({T, T}, {[1 -1 -3], [1 -2 -4]});
-  transfer_matrix = ncon({transfer_matrix, transfer_matrix}, {[1 2 -1 -2], [1 2 -3 -4]});
+  transfer_matrix = ncon({T, construct_a(temperature), T}, {[1 -1 -4], [1 -2 2 -5], [2 -3 -6]});
 
-  % reshape into chi^2 x chi^2 matrix
-  [transfer_matrix, ~, ~] = lreshape(transfer_matrix, [1 2], [3 4]);
+  % reshape into 2chi^2 x 2chi^2 matrix
+  [transfer_matrix, ~, ~] = lreshape(transfer_matrix, [1 2 3], [4 5 6]);
 
   eigenvalues = eigs(transfer_matrix, 2);
   correlation_length = 1 / log(eigenvalues(1) / eigenvalues(2));
--- MATLAB/Scripts/plot_correlation_length_vs_T.m
+++ MATLAB/Scripts/plot_correlation_length_vs_T.m
@@ -1,7 +1,7 @@
 function plot_correlation_length_vs_T()
   temperature_width = 0.001;
   % 2.273435314213022
-  T_pseudocrit = T_crit
+  T_pseudocrit = T_crit;
   temperatures = linspace(T_pseudocrit - temperature_width, T_pseudocrit + temperature_width, 9);
 
   chi_values = [9, 11, 13, 15];
@@ -22,6 +22,6 @@ function plot_correlation_length_vs_T()
   axis manual
   line([T_crit, T_crit], [0, 100000], 'LineStyle', '--');
   % line([T_pseudocrit, T_pseudocrit], [0, 100000], 'LineStyle', '--');
-  export_fig('../correlation_length_chi9-15_tol1e-9_width1e-3.pdf')
+  % export_fig('../correlation_length_chi9-15_tol1e-9_width1e-3.pdf')
 
 end
